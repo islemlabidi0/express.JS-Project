960655c0338416c8a2b01ac2731baf3430c0d028
Islem Labidi <labidiislem971@gmail.com>
2025-12-02 11:28:19 +0100

    new feature: delete user

--- UserController.js
+++ UserController.js
@@ -84,4 +84,22 @@ const updateUser = async(req,res) => {
         res.status(500).json({error: error.message});
     }
 };
-module.exports = { createUser, updateUser };
+
+//delete user 
+const deleteUser = async(req, res)=>{
+    try{
+        const userId = req.params.id;
+
+        const user = await User.findById(userId);
+        if(!user){
+            return res.status(404).json({message: "User not found"});
+        }
+
+        await User.findByIdAndDelete(userId);
+
+        res.status(200).json({message: "User delete successfully! "});
+    }catch(error){
+        res.status(500).json({error: error.message});
+    }
+}
+module.exports = { createUser, updateUser , deleteUser};
--- UserRoute.js
+++ UserRoute.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { createUser } = require('./UserController.js');
 const validateUser = require('./validateUser.js');
-const { updateUser } = require('./UserController.js')
+const { updateUser } = require('./UserController.js');
+const { deleteUser } = require('./UserController.js');
 
 router.post('/users', validateUser ,createUser);
 router.put('/users/:id', validateUser, updateUser);
+router.delete('/users/:id',deleteUser);
 
 module.exports = router;
