677398d51286cd8fc48e99471562068d23fa7484
Rahma Hdhili <rahma1.benhdhili@gmail.com>
2025-12-02 23:13:11 +0100

    register and login check

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -24,7 +24,7 @@ router.get('/activate/:token', async (req, res) => {
 
   res.status(200).json({ message: "Account activated successfully" });
 });
-router.post('/login', authController.login);
+
 
 
 module.exports = router;
